cec288dbc3693f615d7e69e4cd29e86bf78733ef
Andhika Pramana <andhikapramana101@gmail.com>
2022-08-16 10:58:37 +0700

    Add meta

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,14 +1,48 @@
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
-function Layout({ children, className }) {
+function Layout({ children, ...customMeta }) {
+  const router = useRouter();
+  const meta = {
+    title: "Andhika Pramana Putra - Web Developer",
+    description: `A 24-year-old web developer based in Jombang, Indonesia.`,
+    image: "/andhikapramana.png",
+    type: "website",
+    ...customMeta,
+  };
   return (
-    <div className="min-h-screen bg-white text-slate-700">
-      <Navbar />
-      {children}
-      {/* <Footer /> */}
-    </div>
+    <>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta
+          name="google-site-verification"
+          content="GPht8NK2bfNlSiWaQwNg-_feRA_eSzUbA9pHrRmUpiE"
+        />
+        <meta content={meta.description} name="description" />
+        <meta
+          property="og:url"
+          content={`https://andhikapramana.my.id/${router.asPath}`}
+        />
+        <link
+          rel="canonical"
+          href={`https://andhikapramana.my.id/${router.asPath}`}
+        />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Andhika Pramana" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+      </Head>
+      <div className="min-h-screen bg-white text-slate-700">
+        <Navbar />
+        {children}
+        {/* <Footer /> */}
+      </div>
+    </>
   );
 }
 
