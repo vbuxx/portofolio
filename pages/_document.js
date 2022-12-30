9eb02f091f97ad60c5a189e4adaa91ece881f080
Andhika Pramana <106736531+vbuxx@users.noreply.github.com>
2022-12-30 23:00:34 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,15 @@ class MyDocument extends Document {
             name="google-site-verification"
             content="gO2UedgoT25s2fLlaofsgPAE3r6jq_nHb83d16nwvLY"
           />
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DDMRTQS18H"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GA_TRACKING_ID');
+        </script>
         </Head>
         <body className="text-slate-800 dark:text-slate-100 dark:bg-slate-900">
           <Main />
