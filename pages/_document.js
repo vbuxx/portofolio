ec36b81a413493a25cef5e47aeb557fc485928ec
Andhika Pramana <106736531+vbuxx@users.noreply.github.com>
2023-01-01 19:19:35 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,15 +11,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="google-site-verification" content="pRIFpS4jHzp1SdOgd8MF7NhJaxKy9IljcJyNMj2dteI" />
-         <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DDMRTQS18H"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-DDMRTQS18H');
-        </script>
         </Head>
         <body className="text-slate-800 dark:text-slate-100 dark:bg-slate-900">
           <Main />
