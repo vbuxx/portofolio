1d9bc6368bebd0ed603d76af922da1fedce65769
Andhika Pramana <106736531+vbuxx@users.noreply.github.com>
2023-03-18 22:47:45 +0700

    Update gtag

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script"
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -11,16 +12,19 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="google-site-verification" content="pRIFpS4jHzp1SdOgd8MF7NhJaxKy9IljcJyNMj2dteI" />
-      
-          <!-- Google tag (gtag.js) -->
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-C6J8M7NT3R"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-C6J8M7NT3R"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-            gtag('config', 'G-C6J8M7NT3R');
-          </script>
+              gtag('config', 'GA_MEASUREMENT_ID');
+            `}
+          </Script>
 
         </Head>
         <body className="text-slate-800 dark:text-slate-100 dark:bg-slate-900">
