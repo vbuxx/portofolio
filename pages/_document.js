370d87855b78258becc937041510502fd9540f56
Andhika Pramana <106736531+vbuxx@users.noreply.github.com>
2023-03-18 22:36:01 +0700

    Add gtag for google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,17 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="google-site-verification" content="pRIFpS4jHzp1SdOgd8MF7NhJaxKy9IljcJyNMj2dteI" />
+      
+          <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-C6J8M7NT3R"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-C6J8M7NT3R');
+          </script>
+
         </Head>
         <body className="text-slate-800 dark:text-slate-100 dark:bg-slate-900">
           <Main />
